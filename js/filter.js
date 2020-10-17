19311436fa46b151225be3b82ea29c660e527ded
dmitry-ios <60504537+dmitry-ios@users.noreply.github.com>
2020-10-17 16:49:36 +0300

    Упрощает способ фильтрации объявлений

--- js/filter.js
+++ js/filter.js
@@ -1,25 +1,20 @@
 'use strict';
 
 (function () {
-  const doNothing = function () {
-  };
-
-  let filter = {
-    onTypeChange: doNothing
-  };
-
   const map = document.querySelector(`.map`);
   const housingTypeSelect = map.querySelector(`select[name=housing-type]`);
 
-  housingTypeSelect.addEventListener(`change`, function () {
-    filter.onTypeChange(housingTypeSelect.value);
-  });
+  const getHousingType = (element) => {
+    return housingTypeSelect.value === window.data.ANY_TYPE ? true : element.offer.type === housingTypeSelect.value;
+  };
 
-  const setTypeChangeHandler = function (cb) {
-    filter.onTypeChange = cb;
+  const getFilteredOffers = (offers) => {
+    return offers.filter((element) => {
+      return getHousingType(element);
+    });
   };
 
   window.filter = {
-    setTypeChangeHandler
+    getFilteredOffers
   };
 })();
--- js/map.js
+++ js/map.js
@@ -4,10 +4,10 @@
   let isActivePage = true;
 
   let offers = [];
-  let housingType = window.data.ANY_TYPE;
 
   const map = document.querySelector(`.map`);
   const mapPins = document.querySelector(`.map__pins`);
+  const mapFiltersForm = document.querySelector(`.map__filters`);
 
   const successLoadHandler = function (jsonData) {
     offers = jsonData;
@@ -35,17 +35,12 @@
     hideMapPins();
     window.card.hideCard();
 
-    const filteredOffers = offers.filter((offerInfo) => {
-      return housingType === window.data.ANY_TYPE || offerInfo.offer.type === housingType;
-    });
+    const filteredOffers = window.filter.getFilteredOffers(offers);
 
     window.render.setupSimilarPins(filteredOffers);
   };
 
-  window.filter.setTypeChangeHandler(window.util.debounce(function (type) {
-    housingType = type;
-    updatePins();
-  }));
+  mapFiltersForm.addEventListener(`change`, window.util.debounce(updatePins));
 
   window.map = {
     isActivePage,
