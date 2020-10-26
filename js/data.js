c234ae20ba624f474dd593e9886126730e939f4a
dmitry-ios <60504537+dmitry-ios@users.noreply.github.com>
2020-10-26 08:32:36 +0300

    Удаляет неиспользуемый код

--- js/data.js
+++ js/data.js
@@ -1,23 +1,5 @@
 'use strict';
 
-const TITLES = [
-  `Милая, уютная квартира в центре Токио`,
-  `Футуристическое жилье в современном районе`,
-  `Дворец с роботами`,
-  `Личный дом с котиками и собачками`,
-  `Бунгало со всеми удобствами`,
-  `Маленькая комнатка со скоростным интернетом`,
-  `Большая квартира для большой компании`,
-  `Романтическое гнездышко в городе будущего`
-];
-
-const TYPES = [
-  `palace`,
-  `flat`,
-  `house`,
-  `bungalow`
-];
-
 const ANY_TYPE = `any`;
 
 const valuesType = {
@@ -34,27 +16,6 @@ const minPrice = {
   'palace': 10000
 };
 
-const TIMES = [
-  `12:00`,
-  `13:00`,
-  `14:00`
-];
-
-const FEATURES = [
-  `wifi`,
-  `dishwasher`,
-  `parking`,
-  `washer`,
-  `elevator`,
-  `conditioner`
-];
-
-const PHOTOS = [
-  `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
-  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
-  `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
-];
-
 const roomValidityMessage = {
   1: `1 комната — «для 1 гостя»`,
   2: `2 комнаты — «для 2 гостей» или «для 1 гостя»`,
@@ -65,14 +26,9 @@ const roomValidityMessage = {
 let offers = [];
 
 window.data = {
-  TITLES,
-  TYPES,
   ANY_TYPE,
   valuesType,
   minPrice,
-  TIMES,
-  FEATURES,
-  PHOTOS,
   roomValidityMessage,
   offers
 };
--- js/util.js
+++ js/util.js
@@ -1,34 +1,5 @@
 'use strict';
 
-const randomRange = function (min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
-
-const shuffleArray = function (array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * i);
-    const t = array[i];
-    array[i] = array[j];
-    array[j] = t;
-  }
-};
-
-const makeElements = function (limit, producer) {
-  let elements = [];
-
-  for (let i = 0; i < limit; i++) {
-    elements[i] = producer(i);
-  }
-
-  return elements;
-};
-
-const generateAvatarUrl = function (index) {
-  index += 1;
-  let suffix = (index < 10) ? `0${index}` : index;
-  return `img/avatars/user${suffix}.png`;
-};
-
 const errorHandler = function (errorMessage) {
   const node = document.createElement(`div`);
 
@@ -79,10 +50,6 @@ const debounce = function (cb) {
 };
 
 window.util = {
-  randomRange,
-  shuffleArray,
-  makeElements,
-  generateAvatarUrl,
   errorHandler,
   limitMapX,
   limitMapY,
